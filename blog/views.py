032a3b0f5ec21d2035973fc1386c8122f3a9486e
chandsgowda <chandansuresh007@gmail.com>
2022-01-23 23:37:21 +0530

    removed post_data dictionary

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-posts_data = [
-    {
-        "title": "How to learn Python in Kannada?",
-        "content": "Go to my youtube channel and learn python in kannada.Videos are already Uploaded",
-        "slug": "how-to-learn-python-kannada"
-    },
-    {
-        "title": "How to learn Python in English?",
-        "content": "Go to my youtube channel and learn python in english. Videos will be uploaded soon.",
-        "slug": "how-to-learn-python-english"
-    },
-    {
-        "title": "How to post a free ad on pgfy",
-        "content": "Pgfy has a free plan where you can post any one ad for free for 20 days. Go to www.pgfy.in for more info",
-        "slug": "how-to-post-free-ad-on-pgfy"
-    },
-]
-
 # Create your views here.
 
 def index(request):
