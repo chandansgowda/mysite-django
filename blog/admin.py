bf5112581cd0368c6005a25bd1b943eb40472a82
chandsgowda <chandansuresh007@gmail.com>
2022-01-23 23:36:34 +0530

    models and admin setup

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from blog.models import Author, Post, Tags
+
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+    list_display = ("title","author", "slug", "date")
+    list_filter = ("author", "tags", "date")
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tags)
+admin.site.register(Author)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Tags(models.Model):
+    tag = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.tag}"
+
+    class Meta:
+        verbose_name_plural = "Tags"
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(default="", blank=True, null=False,db_index=True)
+    content = models.CharField(max_length=20000)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null = True)
+    tags = models.ManyToManyField(Tags)
+    excerpt = models.CharField(max_length=5000)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
